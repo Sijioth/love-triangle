20831c55b51be521bc9d1396082add14a7058c10
Sijioth <jhnsmthmit@gmail.com>
2019-02-19 05:48:07 +0300

    master/fix: Add implementation for the love-triangle function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  // My implementation of the function
+  var count = 0; 
+  var arr = preferences.slice();
+  arr.unshift('shifted by 1');
+  var n, k, l = 0;
+  for (n = 1; n < arr.length; n++){
+  	k = arr[n], l = arr[k];
+    if (k != l && arr[k] == l && arr[l] == n && arr[n] == k) {
+    	count++;
+    	delete(arr[n]);
+    }
+  }
+return count;
 };
